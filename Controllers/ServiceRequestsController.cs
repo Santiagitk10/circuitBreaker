d00a71b229defe4f81b8bbffe8b3f56c7245a357
Santiago Sierra Herrera <santiago.sierra@sofka.com.co>
2023-07-17 11:26:14 -0500

    circuit breaker implementation

--- Controllers/ServiceRequestsController.cs
+++ Controllers/ServiceRequestsController.cs
@@ -17,29 +17,93 @@ public class ServiceRequestsController: ControllerBase
     public async Task<IActionResult> Get()
     {
 
+        //RETRY POLICY
         //Se puede implementar una política por cada tipo de excepción, en este caso 
         //se hace para una excepción en general
-        var retryPolicy = retryPolic
+        //var retryPolicy = Policy
+        //    .Handle<Exception>()
+        //    .RetryAsync(5, onRetry: (exception, retryCount) =>
+        //    {
+        //        Console.WriteLine("Error:" + exception.Message + " ... Retry Count " + retryCount);
+        //    });
+
+
+        //await retryPolicy.ExecuteAsync(async () =>
+        //{
+        //    await ConnectToApi();
+        //})
+
+
+        var amountToPause = TimeSpan.FromSeconds(15);
+
+
+        //RETRY AND WAIT POLICY
+        //var retryWaitPolicy = Policy
+        //    .Handle<Exception>()
+        //    .WaitAndRetryAsync(5, i => amountToPause, onRetry: (exception, retryCount) =>
+        //    {
+        //        Console.WriteLine("Error:" + exception.Message + " ... Retry Count " + retryCount)
+        //    })
+
+        //await retryWaitPolicy.ExecuteAsync( async () =>
+        //{
+        //    await ConnectToApi();
+        //})
+
+
+
+         var retryPolicy = Policy
             .Handle<Exception>()
-            .RetryAsync(5, onRetry: (exception, retryCount) =>
+            .WaitAndRetryAsync(5, i => amountToPause, onRetry: (exception, retryCount) =>
             {
-                Console.WriteLine("Error:" + exception.Message + " ... Retry Count " + retryCount);
-            });
+                Console.WriteLine("Error:" + exception.Message + " ... Retry Count " + retryCount)
+            })
 
+        var circuitBreakerPolicy = Policy
+            .Handle<Exception>()
+            .CircuitBreaker(3, TimeSpan.FromSeconds(30));
 
-        await retryPolicy.ExecuteAsync(async () =>
+        var finalPolicy = retryPolicy.Wrap(circuitBreakerPolicy);
+
+        finalPolicy.Execute(() =>
         {
-            await ConnectToApi();
+            Console.WriteLine("Excecuting");
+            ConnectToApi();
         })
-        
+         
 
 
-      
         return Ok();
     }
 
 
-    private async Task ConnectToApi()
+    //METODO ASÍNCRONO
+    //private async Task ConnectToApi()
+    //{
+    //    var url = "https://chuck-norris-jokes.p.rapidapi.com/de/jokes/random";
+
+    //    var client = new RestClient();
+    //    var request = new RestRequest(url, Method.GET);
+    //    request.AddHeader("X-RapidAPI-Key", "SIGN-UP-FOR-KEY");
+    //    request.AddHeader("X-RapidAPI-Host", "chuck-norris-jokes.p.rapidapi.com");
+    //    IRestResponse response =  await client.ExecuteAsync(request);
+
+
+    //    if(response.IsSuccessful)
+    //    {
+    //        Console.WriteLine(response.Content);
+    //    }
+    //    else
+    //    {
+    //        Console.Write(response.ErrorMessage);
+    //        throw new Exception("Unable to connect to the service");
+    //    }
+
+    //}
+
+
+    //MÉTODO SÍNCRONO
+    private void ConnectToApi()
     {
         var url = "https://chuck-norris-jokes.p.rapidapi.com/de/jokes/random";
 
@@ -47,10 +111,10 @@ public class ServiceRequestsController: ControllerBase
         var request = new RestRequest(url, Method.GET);
         request.AddHeader("X-RapidAPI-Key", "SIGN-UP-FOR-KEY");
         request.AddHeader("X-RapidAPI-Host", "chuck-norris-jokes.p.rapidapi.com");
-        IRestResponse response =  await client.Execute(request);
+        IRestResponse response =  client.Execute(request);
 
 
-        if(response.IsSuccessful)
+        if (response.IsSuccessful)
         {
             Console.WriteLine(response.Content);
         }
